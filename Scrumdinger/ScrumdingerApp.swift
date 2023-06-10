8e66dd8bc76a446ef9bf878ace2c4bdb0cd07795
Daniel Li <daniel.li.work@gmail.com>
2023-06-10 23:09:48 +0100

    lesson 2: Using stacks to arrange views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Daniel Li on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}

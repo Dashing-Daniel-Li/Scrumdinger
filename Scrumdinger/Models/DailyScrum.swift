222516a8c4cedaf0af12cf03a0544296c34df5b0
Daniel Li <daniel.li.work@gmail.com>
2023-06-25 13:21:24 +0100

    Lesson 5 'creating-a-navigation-hierarchy'

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -10,17 +10,32 @@ import Foundation
 struct DailyScrum : Identifiable {
     let id: UUID
     var title:String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
+
+    
     
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
             self.id = id
             self.title = title
-            self.attendees = attendees
             self.lengthInMinutes = lengthInMinutes
             self.theme = theme
+            self.attendees = attendees.map { Attendee(name: $0) }
+        
         }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+        
+        init(id: UUID = UUID(), name: String) {
+            self.id = id
+            self.name = name
+        }
+    }
 }
 
 extension DailyScrum {
